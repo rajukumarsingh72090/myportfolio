fe87c66a9607c6c125d34f6be5ca8e13cc3bc96e
rajukumarsingh72090 <rajukumarsingh72090@gmail.com>
2023-11-29 15:09:05 +0530

    my portfolio website

---
+++ script.js
@@ -0,0 +1,39 @@
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabName) {
+  for (tablink of tabLinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabName).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzEsUg7VHFWDdm4ri9MyS9PxyPHzbRflJJNGaph36X6q1Nd07REL7pgEaMlYi4srxxE/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "message successfully sent";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 1000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
